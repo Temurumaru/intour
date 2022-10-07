2678069c4d5064c1767ce5eb4e7c87883687c9e2
Temurumaru <temurumaru@yandex.com>
2022-10-07 11:22:17 +0500

    modified:   app/Http/Controllers/AdminController.php 	modified:   public/lang/en.php 	modified:   public/lang/ru.php 	modified:   resources/views/about.blade.php 	modified:   resources/views/admin.blade.php 	modified:   resources/views/admin_blog.blade.php 	modified:   resources/views/admin_blog_upd.blade.php 	modified:   resources/views/admin_cars.blade.php 	modified:   resources/views/admin_cities_blog.blade.php 	modified:   resources/views/admin_creator.blade.php 	modified:   resources/views/admin_gallery.blade.php 	modified:   resources/views/admin_signin.blade.php 	modified:   resources/views/admin_tour.blade.php 	modified:   resources/views/admin_tour_upd.blade.php 	modified:   resources/views/admin_tourism.blade.php 	modified:   resources/views/blog.blade.php 	modified:   resources/views/blog_sub.blade.php 	modified:   resources/views/car.blade.php 	modified:   resources/views/contact.blade.php 	modified:   resources/views/destinations.blade.php 	modified:   resources/views/gallery.blade.php 	modified:   resources/views/gallery_sub.blade.php 	modified:   resources/views/home.blade.php 	modified:   resources/views/tour.blade.php 	modified:   resources/views/tour_cities.blade.php 	modified:   routes/web.php
    Untracked files:
    	resources.zip
    	resources/views/admin_home_wallpaper.blade.php
    	resources/views/visa.blade.php
    	zzz.zip

--- resources/views/tourism.blade.php
+++ resources/views/tourism.blade.php
@@ -18,7 +18,7 @@ $gallery = json_decode($data -> img, true);
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Intour</title>
+  <title>Intour/Tourism</title>
   @include('blocks.head_imp')
 </head>
 
